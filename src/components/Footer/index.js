66866b7de9d399efb1ea13391b1af18994f19220
Callum Quirk <callumbquirk@gmail.com>
2023-03-18 16:58:59 +0000

    committing changes to footer, where text now glows green and changes in size when hovered over

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,28 +8,28 @@ const Footer = () => {
                 <section className="mt-5">
                     <div className="row text-center d-flex justify-content-center">
                         <div className="col-4">
-                            <h6 className="font-weight-bold">
-                                <a href="/">Home</a>
-                            </h6>
+                                <a href="/">
+                                    <h4 className="font-weight-bold">Home</h4>
+                                </a>
                         </div>
                         <div className="col-4">
-                            <h6 className="font-weight-bold">
-                                <a href="/projects">Projects</a>
-                            </h6>
+                                <a href="/projects">
+                                    <h4 className="font-weight-bold">Projects</h4>
+                                </a>
                         </div>
                         <div className="col-4">
-                            <h6 className="font-weight-bold">
-                                <a href="/contact">Contact</a>
-                            </h6>
+                        <a href="/contact">
+                            <h4 className="font-weight-bold">Contact</h4>
+                        </a>
                         </div>
                     </div>
                 </section>
                 <section className="socials mb-5">
                     <a href="linkedin.com/in/callum-quirk-ba6269150" class="text-white me-4">
-                        <img className="footer-social" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" />
+                        <img className="footer-social" alt="Callum Quirk LinkedIn" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" />
                     </a>
                     <a href="https://github.com/callumquirky" class=" me-4">
-                        <img className="footer-social" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
+                        <img className="footer-social" alt="Callum Quirk Github" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />
                     </a>
                 </section>
             </div>
