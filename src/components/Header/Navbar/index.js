f2c836d63cfb8a5bfd901cc8f7e97ec5250651a7
Callum Quirk <callumbquirk@gmail.com>
2023-03-20 23:19:55 +0000

    committing changes to navbar so that the words don't fall out of it when the screen width reduced, also made changes to sidebar so that the first child has a margin to not interrupt with the logo. z-indexes added to priritise the top navbar

--- src/components/Header/Navbar/index.js
+++ src/components/Header/Navbar/index.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav id="top-nav">
             <label className="logo">Callum Quirk</label>
             <ul className="top-nav-ul">
-                <li ><a className="top-nav-link active" href="/">Home</a></li>
+                <li ><a className="top-nav-link" href="/">Home</a></li>
                 <li ><a className="top-nav-link" href="/projects">Projects</a></li>
                 <li ><a className="top-nav-link" href="/contact">Contact</a></li>
             </ul>
