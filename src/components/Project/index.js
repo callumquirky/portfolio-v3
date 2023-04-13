098921814c999f0cf0f05056d99253afe5d5e6f4
Callum Quirk <callumbquirk@gmail.com>
2023-04-13 18:34:48 +0100

    fixed issues with styling on project component, where the hover effect was misaligned

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -4,25 +4,24 @@ import '../Project/project.css'
 
 function Project(props){
     return(
-        <div className="col-sm-4 project-container">
-            <div className="img-container">
-                <img alt={props.name} src={props.image}/>
+        <div className="col-sm-4 ">
+            <div className="project-container">
+                <img className="card-img-top" alt={props.name} src={props.image}/>
+                <div className="project-content">
+                    <h4>
+                        {props.name}
+                    </h4>
+                    <p>
+                        {props.description}
+                    </p>
+                <a className="btn" href={props.deployedLink}>
+                    View Deployed Link
+                </a>
+                <a className ="btn" href={props.repoLink}>
+                    View Repo
+                </a>
+                </div>
             </div>
-            <div className="project-content">
-                <h4>
-                    {props.name}
-                </h4>
-                <p>
-                    {props.description}
-                </p>
-            <a className="btn" href={props.deployedLink}>
-                View Deployed Link
-            </a>
-            <a className ="btn" href={props.repoLink}>
-                View Repo
-            </a>
-            </div>
-
         </div>
     )
 }
