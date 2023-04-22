3857e32ec6f92db0ae01cc6aa1eda66c8a43fb5d
Callum Quirk <callumbquirk@gmail.com>
2023-04-22 12:47:59 +0100

    styling to form to make more uniform

--- src/components/Pages/Contact/index.js
+++ src/components/Pages/Contact/index.js
@@ -7,16 +7,16 @@ const Contacts = () => {
     return(
         <div className="container pt-4 contact-head">
             <h2 id="contact-title">Let's get in touch</h2>
-            <div class="col-lg-6">
-                <form name="contact" netlify>
-                    <input type="text" class="form-control form-control-lg" placeholder="Name"/>
-                    <input type="email" class="form-control mt-3" placeholder="Email"/>
-                    <input type="text" class="form-control mt-3" placeholder="Subject"/>
+            <div class="col-lg-6 text-center contact-form">
+                <form  name="contact" netlify>
+                    <input type="text" name="name" className="form-control form-control-lg" placeholder="Name"/>
+                    <input type="email" name="email" className="form-control mt-3" placeholder="Email"/>
+                    <input type="text" name="subject" className="form-control mt-3" placeholder="Subject"/>
                     <div class="mb-3 mt-3">
-                        <textarea class="form-control" rows="5" id="comment" name="text" placeholder="Project Details"></textarea>
+                        <textarea class="form-control" rows="5" id="comment" name="text" placeholder="Your message"></textarea>
                     </div>
                 </form>
-                <button type="button" class="btn btn-success mt-3">Contact Me</button>
+                <button type="button" className="btn mt-3 form-btn">Contact Me</button>
             </div>
 
             <div className="row contact-column">
