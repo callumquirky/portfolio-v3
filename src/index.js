107e81e97b657afd2cf870c39254ea42faefbaaa
Callum Quirk <callumbquirk@gmail.com>
2023-04-22 14:29:10 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import "bootstrap/dist/js/bootstrap.bundle.min"
 import "bootstrap/dist/css/bootstrap.min.css"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import DevIcon from "devicon-react-svg"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
